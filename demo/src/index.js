b9b0c918e7ab04155a9c3411583341c0bfd3349b
Leo Nicolle <leo@linkurio.us>
2020-09-11 12:00:45 +0200

    step3: style nodes: width, color and icons

--- demo/src/index.js
+++ demo/src/index.js
@@ -39,29 +39,65 @@ session
  * of exchange they represent grows.
  */
 
-// Define buckets of importance for edges
+// Define buckets of importance for nodes
 const edgeSlices = [
-  // [volume, color, size]
   [280, "#444", 3.5],
   [150, "#777", 2],
   [15, "#AAA", 1.5],
   [0, "#DDD", 0.5],
 ];
 
-// Define edge rule:
+/**
+ * Let's style the nodes,
+ * We will set their colors and icons depending on their type.
+ * and their radius will grow accordingly to the total exchanges passing through them
+ */
+const nodeSlices = [
+  [/^Supplier.*/, "#6FC1A7", "\uEA04"],
+  [/RawSupplier|AllRawsuppliers/, "#3DCB8D", "\uEA03"],
+  [/FrameSupplier/, "#6FC1A7", "\uEA05"],
+  [/WheelSupplier/, "#6FC1A7", "\uEA08"],
+  [/Wholesaler|AllWholesalers/, "#E77152", "\uEA09"],
+  [/Retailer|Allretailers/, "#9AD0D0", "\uEA06"],
+  [/Product/, "#76378A", "\uEA02"],
+];
+
 ogma.styles.addRule({
+  // Edges style:
   edgeAttributes: function (edge) {
-    //
     const quantity = edge.getData("neo4jProperties.quantity");
-    const [, color, width] = edgeSlices.find(
-      ([threshold]) => quantity > threshold
-    );
+    //   if (!quantity) return slices[0][1];
+    const [, color, width] = quantity
+      ? edgeSlices.find(([threshold]) => quantity > threshold)
+      : slices[0];
     return {
       shape: {
         head: "arrow",
       },
       color,
       width,
+    };
+  },
+  // Node style:
+  nodeAttributes: function (node) {
+    const type = node.getData("neo4jLabels")[0];
+    const [, color, icon] =
+      nodeSlices.find(([sliceType]) => type.match(sliceType)) || nodeSlices[0];
+    return {
+      icon: {
+        font: "icon-font",
+        color: "#000",
+        content: icon,
+      },
+      color,
+      // nodes size depend on the quantity of product that is exanged through them
+      radius:
+        node
+          .getAdjacentEdges()
+          .reduce((acc, e) => acc + e.getData("neo4jProperties.quantity"), 0) /
+          300 +
+        5,
+      color,
     };
   },
 });
