b857cc82a996bcd57b51b2fb3507d16aa96c7c23
Leo Nicolle <leo@linkurio.us>
2020-09-11 12:00:45 +0200

    added icons

--- demo/src/index.js
+++ demo/src/index.js
@@ -58,48 +58,50 @@ function shouldPulse(node) {
 }
 
 // Define buckets of importance for nodes
-const slices = [
+const edgeSlices = [
   [280, "#444", 3.5],
   [150, "#777", 2],
   [15, "#AAA", 1.5],
   [0, "#DDD", 0.5],
 ];
 
+const nodeSlices = [
+  ["rawsupplier", "#3DCB8D", "\uEA03"],
+  ["supplier", "#6FC1A7", "\uEA04"],
+  ["wholesaler", "#E77152", "\uEA09"],
+  ["retailer", "#9AD0D0", "\uEA06"],
+  ["product", "#76378A", "\uEA02"],
+];
+
 ogma.styles.addRule({
   // Edges style:
-  edgeAttributes: {
-    shape: {
-      head: "arrow",
-    },
-    // edges color and size depend on the quantity
-    // of product exchange they represent
-    color: function (e) {
-      const quantity = e.getData("neo4jProperties.quantity");
-      if (!quantity) return slices[0][1];
-      return slices.find(([threshold]) => quantity > threshold)[1];
-    },
-    width: function (e) {
-      const quantity = e.getData("neo4jProperties.quantity");
-      if (!quantity) return slices[0][2];
-      return slices.find(([threshold]) => quantity > threshold)[2];
-    },
+  edgeAttributes: function (edge) {
+    const quantity = edge.getData("neo4jProperties.quantity");
+    //   if (!quantity) return slices[0][1];
+    const [, color, width] = quantity
+      ? edgeSlices.find(([threshold]) => quantity > threshold)
+      : slices[0];
+    return {
+      shape: {
+        head: "arrow",
+      },
+      color,
+      width,
+    };
   },
   // Node style:
   nodeAttributes: function (node) {
     const type = getNodeType(node);
+    const [, color, icon] =
+      nodeSlices.find(([sliceType]) => sliceType === type) || nodeSlices[0];
     return {
+      icon: {
+        font: "icon-font",
+        color: "#000",
+        content: icon,
+      },
       // define colors depending on the type of the node
-      color:
-        type === "supplier"
-          ? "#6FC1A7"
-          : type === "wholesaler"
-          ? "#E77152"
-          : type === "rawsupplier"
-          ? "#3DCB8D"
-          : type === "product"
-          ? "#76378A"
-          : // retailer
-            "#9AD0D0",
+      color,
       // nodes size depend on the quantity of product that is exanged through them
       radius:
         node
