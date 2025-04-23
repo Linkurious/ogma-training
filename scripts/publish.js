0613457ca2461f7913b14dc8119061b04ec76ee8
w8r <alex@milevski.co>
2025-04-23 11:11:33 +0200

    Publishing

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,19 +1,11 @@
-#!/bin/node
-const rimraf = require("rimraf");
+const { rimraf } = require("rimraf");
 const copy = require("recursive-copy");
 
-new Promise((resolve, reject) => {
-  rimraf('docs', (err) => {
-    if (err) {
-      return reject(err)
-    }
-    resolve()
+rimraf("docs")
+  .then(() => copy("dist", "docs"))
+  .catch(function (error) {
+    console.error("Publish failed: " + error);
   })
-})
-.then(() =>copy('dist', 'docs'))
-.catch(function(error) {
-  console.error('Publish failed: ' + error);
-}).then(() => {
-  console.log('Publish complete. Just commit docs/ and push on master.');
-})
-  
+  .then(() => {
+    console.log("Publish complete. Just commit docs/ and push on master.");
+  });
