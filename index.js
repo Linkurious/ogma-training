9440dc11307d94b09c24219da319d6ee05e5d6f5
w8r <alex@milevski.co>
2025-04-23 11:37:56 +0200

    Updates

---
+++ index.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  Reveal.initialize({
+    plugins: [ RevealHighlight, RevealNotes ],
+    hash: true,
+    slideNumber: true,
+    width: "100%",
+    height: "100%",
+  });
+
+})();
